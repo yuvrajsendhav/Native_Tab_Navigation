a34ea42245ef368c29aec92593c1db39307e22aa
Yuvraj Singh Thakur <104247355+yuvrajsendhav@users.noreply.github.com>
2022-10-13 15:21:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,60 @@
+import * as React from 'react';
+import { Text, View, focused } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icons from 'react-native-vector-icons/MaterialCommunityIcons';
+import Home from './src/Home';
+import Service from './src/Service';
+import About from './src/About';
+// function Home() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Home!</Text>
+//     </View>
+//   );
+// }
+
+// function Service() {
+//   return (
+//     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+//       <Text>Settings!</Text>
+//     </View>
+//   );
+// }
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator 
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === 'Home') {
+            iconName = focused
+              ? 'home'
+              : 'home';
+          } else if (route.name === 'About') {
+            iconName = focused ? 'account-details' : 'account-details';
+          }
+          else if (route.name === 'Service') {
+            iconName = focused ? 'room-service' : 'room-service';
+          }
+
+          // You can return any component that you like here!
+          return <Icons name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'black',
+      })}
+      >
+        <Tab.Screen name="Home" component={Home} />
+        
+        <Tab.Screen name="About" component={About} />
+        <Tab.Screen name="Service" component={Service} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
